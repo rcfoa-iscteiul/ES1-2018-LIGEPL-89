8923aba68c45898b473819bea027aa97474ec602
rcfoa-iscteiul <rcfoa@iscte-iul.pt>
2018-11-19 00:59:03 +0000

    commit 500
    jj

--- twitterexample/src/main/java/twitterexample/TwitterMain.java
+++ twitterexample/src/main/java/twitterexample/TwitterMain.java
@@ -11,35 +11,7 @@ import twitter4j.conf.ConfigurationBuilder;
 public final class TwitterMain  {
 	
 	
-	public static void main(String[] args) {
 
-			
-				try {
-					App frame = new App();
-					frame.setVisible(true);
-					
-		            System.out.println("------------------------\n Showing home timeline \n------------------------");
-		    		
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		
-		
-//public void search() {
-//	int counter=0;
-//	int counterTotal = 0;
-//    for (Status status : statuses) {
-//		if (status.getUser().getName() != null && status.getUser().getName().contains("Correio")) {
-//		//	if(frame.getCheckTwitter().isSelected()) {
-//			frame.getTextarea().append(status.getUser().getName() + ":" + status.getText()+"\n");
-//			counter++;
-//			//}
-//		}
-//		counterTotal++;
-//    }
-//	System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
-//}
 }
 
     
