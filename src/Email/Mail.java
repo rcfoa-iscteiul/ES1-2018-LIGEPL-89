a1546b736e11463ac67ba8deb80ff8b2c431ad97
fmsml-iscteiul <fmsml@iscte-iul.pt>
2018-12-05 04:27:00 +0000

    Mail com ordem cronologica Todos os emails numa arrayList por ordem cronologica com o assunto do email, quem mandou e hora recebido o email

--- src/Email/Mail.java
+++ src/Email/Mail.java
@@ -1,6 +1,7 @@
 package Email;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Folder;
@@ -12,75 +13,71 @@ import javax.mail.Store;
 
 public class Mail {
 	private String combo;
+	private List<String> mails = new ArrayList<String>();
+	private Message message;
 
 	public Mail(String combo) {
-		this.combo=combo;
+		this.combo = combo;
 	}
 
-   public  void check(String host, String storeType, String user, String password) {
-      try {
+	public void check(String host, String storeType, String user, String password) {
+		try {
 
-      //create properties field
-      Properties properties = new Properties();
+			// create properties field
+			Properties properties = new Properties();
 
-      properties.put("mail.pop3.host", host);
-      properties.put("mail.pop3.port", "995");
-      properties.put("mail.pop3.starttls.enable", "true");
-      Session emailSession = Session.getDefaultInstance(properties);
-  
-      //create the POP3 store object and connect with the pop server
-      Store store = emailSession.getStore("pop3s");
+			properties.put("mail.pop3.host", host);
+			properties.put("mail.pop3.port", "995");
+			properties.put("mail.pop3.starttls.enable", "true");
+			Session emailSession = Session.getDefaultInstance(properties);
 
-      store.connect(host, user, password);
+			// create the POP3 store object and connect with the pop server
+			Store store = emailSession.getStore("pop3s");
 
-      //create the folder object and open it
-      Folder emailFolder = store.getFolder("INBOX");
-      emailFolder.open(Folder.READ_ONLY);
+			store.connect(host, user, password);
 
-      // retrieve the messages from the folder in an array and print it
-      Message[] messages = emailFolder.getMessages();
-      System.out.println("messages.length---" + messages.length);
+			// create the folder object and open it
+			Folder emailFolder = store.getFolder("INBOX");
+			emailFolder.open(Folder.READ_ONLY);
 
-      
-    	  for (int i = 0, n=messages.length; i <n; i++) {
-         Message message = messages[i];
-         
-         
-        
-         
-         
-         System.out.println("---------------------------------");
-         System.out.println("Email Number " + (i + 1));
-         System.out.println("Subject: " + message.getSubject());
-         System.out.println("From: " + message.getFrom()[0]);
-         System.out.println("Text: " + message.getContent().toString());
+			// retrieve the messages from the folder in an array and print it
+			Message[] messages = emailFolder.getMessages();
+			System.out.println("messages.length---" + (messages.length - 1));
 
-      }
+			for (int i = messages.length - 1; i > 0; i--) {
+				message = messages[i];
 
-      //close the store and folder objects
-      emailFolder.close(false);
-      store.close();
+				mails.add(message.getSubject()+"-"+message.getFrom()[0].toString() + "-" + message.getSentDate());
+				// mails.add(message.getContent().toString());
+				System.out.println(mails);
+				// System.out.println(mails.size());
 
-      } catch (NoSuchProviderException e) {
-         e.printStackTrace();
-      } catch (MessagingException e) {
-         e.printStackTrace();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
+			}
 
-   public static void main(String[] args) {
+			// close the store and folder objects
+			emailFolder.close(false);
+			store.close();
 
-      String host = "pop.gmail.com";// change accordingly
-      String mailStoreType = "pop3";
-      String username = "rafael.castanheira97@gmail.com";// change accordingly
-      String password = "CORGAS1997";// change accordingly
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-      Mail a= new Mail("Este dia");
-      
-      a.check(host, mailStoreType, username, password);
+	public static void main(String[] args) {
 
-   }
+		String host = "pop.gmail.com";// change accordingly
+		String mailStoreType = "pop3";
+		String username = "bomdiaacademiaiscte@gmail.com";// change accordingly
+		String password = "caninas6969";// change accordingly
+
+		Mail a = new Mail("Este dia");
+
+		a.check(host, mailStoreType, username, password);
+
+	}
 
 }
