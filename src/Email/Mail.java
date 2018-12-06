59065cbd4d635c317be84202bd89c0afc4e9469d
rcfoa-iscteiul <rcfoa@iscte-iul.pt>
2018-12-06 01:41:13 +0000

    Apresentação de Email alterada
    Agora os emails são enumerados de forma mais profissional e organizados
    com data de recebimento, quem enviou e título

--- src/Email/Mail.java
+++ src/Email/Mail.java
@@ -13,16 +13,21 @@ import javax.mail.Store;
 
 public class Mail {
 	private String combo;
-	private List<String> mails = new ArrayList<String>();
+	private ArrayList<String> mails = new ArrayList<String>();
 	private Message message;
 
 	public Mail(String combo) {
 		this.combo = combo;
 	}
 
-	public void check(String host, String storeType, String user, String password) {
+	public void check() {
 		try {
-
+			String host = "pop.gmail.com";
+			String storeType="pop3";
+			String user= "bomdiaacademiaiscte@gmail.com";// change accordingly
+			String password = "caninas6969";
+			
+			
 			// create properties field
 			Properties properties = new Properties();
 
@@ -47,9 +52,9 @@ public class Mail {
 			for (int i = messages.length - 1; i > 0; i--) {
 				message = messages[i];
 
-				mails.add(message.getSubject()+"-"+message.getFrom()[0].toString() + "-" + message.getSentDate());
+				mails.add(message.getSentDate()+"	"+message.getFrom()[0].toString()+"	"+message.getSubject());
 				// mails.add(message.getContent().toString());
-				System.out.println(mails);
+				//System.out.println(mails);
 				// System.out.println(mails.size());
 
 			}
@@ -66,17 +71,15 @@ public class Mail {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	public ArrayList<String> getMails() {
+		return mails;
+	}
 
 	public static void main(String[] args) {
-
-		String host = "pop.gmail.com";// change accordingly
-		String mailStoreType = "pop3";
-		String username = "bomdiaacademiaiscte@gmail.com";// change accordingly
-		String password = "caninas6969";// change accordingly
-
 		Mail a = new Mail("Este dia");
-
-		a.check(host, mailStoreType, username, password);
+		a.check();
 
 	}
 
