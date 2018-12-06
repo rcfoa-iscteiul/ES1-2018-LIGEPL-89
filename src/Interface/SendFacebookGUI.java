7f7d64a7ca6bdb921d66d676c7b4655e33f40c4f
jmmsp-iscteiul <jmmsp@iscte-iul.pt>
2018-12-06 05:52:15 +0000

    Atualização InterfaceFacebookGUI!
    Adicionado Texto, Botão "Send", Botão "Close" e text area

--- src/Interface/SendFacebookGUI.java
+++ src/Interface/SendFacebookGUI.java
@@ -39,7 +39,7 @@ public class SendFacebookGUI extends JFrame {
 	 */
 	public SendFacebookGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 602, 513);
+		setBounds(100, 100, 602, 405);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -52,12 +52,28 @@ public class SendFacebookGUI extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JTextPane textPane = new JTextPane();
-		textPane.setBounds(10, 105, 566, 305);
+		textPane.setBounds(10, 139, 566, 167);
 		contentPane.add(textPane);
 		
 		JButton btnClose = new JButton("Close");
 		btnClose.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnClose.setBounds(195, 421, 230, 23);
+		btnClose.setBounds(305, 317, 230, 23);
 		contentPane.add(btnClose);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(SendFacebookGUI.class.getResource("/Image/facebook3.png")));
+		label.setBounds(495, 11, 70, 62);
+		contentPane.add(label);
+		
+		JLabel lblPleaseWriteThe = new JLabel("Please write the post you want to publish on your Facebook:");
+		lblPleaseWriteThe.setFont(new Font("Arial", Font.BOLD, 14));
+		lblPleaseWriteThe.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseWriteThe.setBounds(10, 105, 566, 23);
+		contentPane.add(lblPleaseWriteThe);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnSend.setBounds(54, 317, 230, 23);
+		contentPane.add(btnSend);
 	}
 }
