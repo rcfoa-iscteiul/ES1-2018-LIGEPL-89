746daf03a1e5c3ef90791f7bfd24bb43012e51a7
jmmsp-iscteiul <jmmsp@iscte-iul.pt>
2018-12-07 04:49:35 +0000

    Interface LoginGUI atualizada!
    Implementação de JavaDoc e revisão no código.

--- src/Interface/LoginGUI.java
+++ src/Interface/LoginGUI.java
@@ -17,11 +17,8 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-import com.sun.glass.events.WindowEvent;
-
 import xpath.InfoXPath;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class LoginGUI.
  */
@@ -29,24 +26,18 @@ public class LoginGUI extends JFrame {
 
 	/** The content pane. */
 	private JPanel contentPane;
-	
+
 	/** The txt username. */
 	private JTextField txtUsername;
-	
+
 	/** The txt password. */
 	private JPasswordField txtPassword;
-	
-	/** The label. */
-	private JLabel label;
-	
-	/** The login. */
-	//private InfoXPath xpath =new InfoXPath();
-	private ArrayList<String> login=InfoXPath.getInstance().getTokens();
-	
-	/** The w. */
-	private WelcomeGUI w=new WelcomeGUI(this);
-	
 
+	/** The login. */
+	private ArrayList<String> login = InfoXPath.getInstance().getTokens();
+
+	/** The w. */
+	private WelcomeGUI w = new WelcomeGUI(this);
 
 	/**
 	 * The main method.
@@ -57,51 +48,47 @@ public class LoginGUI extends JFrame {
 		LoginGUI login = new LoginGUI();
 		login.run();
 	}
-	
-	
-/**
- * Run.
- */
-public  void run() {
-				try {
-					LoginGUI frame = new LoginGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-}
+
+	/**
+	 * Run.
+	 */
+	public void run() {
+		try {
+			LoginGUI frame = new LoginGUI();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public LoginGUI() {
-	//	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+
 		setBounds(100, 100, 516, 338);
 		contentPane = new JPanel();
 		contentPane.setBackground(SystemColor.control);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel label = new JLabel("");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setIcon(new ImageIcon(LoginGUI.class.getResource("/Image/bomdia2.jpg")));
 		label.setBounds(10, 33, 480, 55);
 		contentPane.add(label);
-		
+
 		JLabel lblUsername = new JLabel("Username:");
 		lblUsername.setFont(new Font("Arial", Font.PLAIN, 15));
 		lblUsername.setBounds(72, 127, 83, 31);
 		contentPane.add(lblUsername);
-		
+
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setFont(new Font("Arial", Font.PLAIN, 15));
 		lblPassword.setBounds(72, 182, 83, 31);
 		contentPane.add(lblPassword);
-		
-		
-		
+
 		JButton btnClose = new JButton("Close");
 		btnClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -110,49 +97,54 @@ public  void run() {
 		});
 		btnClose.setBounds(320, 236, 89, 31);
 		contentPane.add(btnClose);
-		
+
 		txtUsername = new JTextField();
 		txtUsername.setFont(new Font("Arial", Font.PLAIN, 15));
 		txtUsername.setBounds(155, 129, 234, 26);
 		contentPane.add(txtUsername);
 		txtUsername.setColumns(10);
-		
+
 		txtPassword = new JPasswordField();
 		txtPassword.setFont(new Font("Arial", Font.PLAIN, 15));
 		txtPassword.setBounds(155, 184, 234, 26);
 		contentPane.add(txtPassword);
-		
+
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String pass=new String(txtPassword.getPassword());
-				login=InfoXPath.getInstance().display(txtUsername.getText(), pass);
-				
-				if(login.isEmpty() || txtUsername.getText().equals("")) {
-					JOptionPane.showMessageDialog(null, "Invalid Login Details", "Login System", JOptionPane.ERROR_MESSAGE);
-
-				}else {
-					w.setUser(txtUsername.getText());
-					w.constroiJanela();
-					dispose();
-					 
-				}
-				
-				
+				loginClicked();
 			}
 		});
 		btnNewButton.setBounds(85, 236, 89, 31);
 		contentPane.add(btnNewButton);
 	}
-	
+
+	/**
+	 * Action performed when client presses the login button.
+	 */
+	public void loginClicked() {
+		String pass = new String(txtPassword.getPassword());
+		login = InfoXPath.getInstance().display(txtUsername.getText(), pass);
+
+		if (login.isEmpty() || txtUsername.getText().equals("")) {
+			JOptionPane.showMessageDialog(null, "Invalid Login Details", "Login System", JOptionPane.ERROR_MESSAGE);
+
+		} else {
+			w.setUser(txtUsername.getText());
+			w.constroiJanela();
+			dispose();
+
+		}
+
+	}
+
 	/**
 	 * Gets the login list.
 	 *
-	 * @return the login
+	 * @return the login list.
 	 */
 	public ArrayList<String> getLogin() {
 		return login;
 	}
-	
-	
+
 }
